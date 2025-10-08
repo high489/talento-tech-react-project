0504e9c5dafcfb58e9ade88fe398a98e0b44d90e
high489 <vitalii.matskaniuk@gmail.com>
2025-10-08 17:20:29 -0300

    Fix: small fixes of styling

--- src/ui/CartItem/index.jsx
+++ src/ui/CartItem/index.jsx
@@ -6,7 +6,7 @@ const CartItem = ({ item }) => {
     <ListGroup.Item className={`${styles['cart-item']}`}>
       <div className='d-flex justify-content-between'>
         <span>{item.title}</span>
-        <span>{item.quantity} {item.quantity > 1 ? 'uds.' : 'ud.'} - {item.quantity * item.price} USD</span>
+        <span>{item.quantity} {item.quantity > 1 ? 'uds.' : 'ud.'} - {(item.quantity * item.price).toFixed(2)} USD</span>
       </div>
     </ListGroup.Item>
   )
